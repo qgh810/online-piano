f284d4dee05503e4ae40d31a9d31ffa00fa79c82
邱国辉 <530784837@qq.com>
2018-05-13 14:44:50 +0800

    完成

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,73 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="control">
+      <input type="checkbox" v-model="full" id="full"><label for="full">完整键盘</label>
+    </div>
+    <div class="keyboard">
+      <group :group="0" v-show="full"></group>
+      <group :group="1" v-show="full"></group>
+      <group :group="2"></group>
+      <group :group="3"></group>
+      <group :group="4"></group>
+      <group :group="5" v-show="full"></group>
+      <group :group="6" v-show="full"></group>
+    </div>
   </div>
 </template>
 
 <script>
+import Group from './components/group'
+import {notes} from 'static/notes.js'
+const prefix = 'data:audio/mpeg;base64,'
+var attachFastClick = require('fastclick')
+attachFastClick.attach(document.body)
+const groupKeys = 12
+const base = 2
+
 export default {
-  name: 'App'
+  components: {
+    Group
+  },
+
+  data () {
+    return {
+      full: false
+    }
+  },
+  methods: {
+    play (keyCode) {
+      let keys = [90, 88, 67, 86, 66, 78, 77, 65, 83, 68, 70, 71, 72, 74, 81, 87, 69, 82, 84, 89, 85]
+      if (keys.indexOf(keyCode) < 0) return
+      let whites = [0, 2, 4, 5, 7, 9, 11]
+      let index = base + 2 * groupKeys + whites[keys.indexOf(keyCode) % 7] + parseInt(keys.indexOf(keyCode) / 7) * groupKeys
+      let audio = new Audio(prefix + notes[index])
+      audio.play()
+    }
+  },
+
+  mounted () {
+    document.body.addEventListener('keydown', e => {
+      this.play(e.keyCode)
+    })
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+  height: 100%;
+}
+body {
+    padding: 0;
+    margin: 0;
+}
+
+.keyboard {
+    display: flex;
+    margin: 20px;
+}
+.control {
+    text-align: center;
+    margin-top: 50px;
 }
 </style>
